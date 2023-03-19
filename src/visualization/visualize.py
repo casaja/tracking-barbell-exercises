40440cec384ab5068320bbce9e8a8bf996b984e3
javiers.castillo <casaja@gmail.com>
2023-03-19 19:29:44 +0100

    Plot multiple axis

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -70,10 +70,24 @@ participant_df.groupby(["participant"])["acc_y"].plot()
 ax.set_ylabel("acc_y")
 ax.set_xlabel("samples")
 plt.legend()
+
 # --------------------------------------------------------------
 # Plot multiple axis
 # --------------------------------------------------------------
 
+label = "squat"
+participant = "A"
+all_axis_df = (
+    df.query(f"label == '{label}'")
+    .query(f"participant == '{participant}'")
+    .reset_index()
+)
+
+fig, ax = plt.subplots()
+all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax)
+ax.set_ylabel("acc_y")
+ax.set_xlabel("samples")
+plt.legend()
 
 # --------------------------------------------------------------
 # Create a loop to plot all combinations per sensor
